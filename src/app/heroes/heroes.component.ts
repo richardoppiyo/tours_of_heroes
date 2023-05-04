463eb265445cff72066dc84eb64fcbe426829b1d
romenyo <richardoppiyo@gmail.com>
2023-05-04 15:18:19 +0300

    filtering individual hero for view

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import { Hero } from '../hero';
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -9,9 +9,14 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent {
-  heroes = HEROES;
-  // hero: Hero = {
-  // id: 1,
-  // name:'Windstorm'
-// }
+  heroes = HEROES ;
+  
+  selectedHero?:Hero; //This is a ternary operator that assign nothing at the begining and a selectged hero when the function is invoke
+
+  //This is an arrow function that gets executed. Pass the hero 
+  onSelect = (hero: Hero): void => {
+  this.selectedHero = hero;
 }
+
+}
+
