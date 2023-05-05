6cf1586d64e19e2a6c2892ed21ae53fdd3057f44
romenyo <richardoppiyo@gmail.com>
2023-05-05 08:47:34 +0300

    changes

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: Hero;
 }
