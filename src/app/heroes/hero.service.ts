4095889ada385210319c856b81911adefdcf225b
romenyo <richardoppiyo@gmail.com>
2023-05-09 11:41:19 +0300

    adding heroes services

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from '../mock-heroes';
+import { Observable, of } from 'rxjs';
 import { LoggerService } from '../logger.service'
+import { Hero } from '../hero';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +10,15 @@ import { LoggerService } from '../logger.service'
 
 export class HeroService {
 
-  getHeroes() {
-    this.logger.log('Getting heroes ...');
-    return HEROES;
-   }
+  // getHeroes() {
+  //   this.logger.log('Getting heroes ...');
+  //   return HEROES;
+  //  }
+
+   getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
+  }
 
   constructor(private logger: LoggerService) { }
 }
