30bc58da1141778995dcff023751c038b18a0742
romenyo <richardoppiyo@gmail.com>
2023-05-09 12:23:30 +0300

    add dashboard and redirects

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: [ './dashboard.component.css' ]
 })
-export class DashboardComponent {
 
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
+
+  constructor(private heroService: HeroService) { }
 }
